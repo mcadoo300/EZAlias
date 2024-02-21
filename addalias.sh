db4c66365c048505ee276c457708db8dec4b5719
Marc McAdoo <marcmcadoo123@gmail.com>
2024-02-21 08:41:51 -0500

    Init with basic adding feature

---
+++ addalias.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+# directory were you keep your bash aliases
+rc='/home/marc/.zshrc'
+# regex for validating alias name
+valid_alias='^[a-zA-Z_][a-zA-Z0-9_]*$'
+# valid arg counts for input checking
+add_arg_count=3
+arg_count=$#
+# ensure at least one argument was passed
+if [ $# -eq 0 ]; then
+	echo "Error: No arguments were passed.\nExample: eza -a test \"echo 'this is a test' \" "
+elif [[ "$1" == "-a" || "$1" == "-am" ]]; then # add alias
+	if [ $# -eq $add_arg_count ]; then
+		local alias_name=$2
+		local command=$3
+		if [[ $alias_name =~ $valid_alias ]]; then
+
+			local exists=$(grep -c "^alias $alias_name=" $rc)
+			echo "$exists"
+			if [ $exists -ne 0 ]; then
+				echo "Error: An alias with that name already exists:"
+				grep "^alias $alias_name=" $rc
+			else
+				echo "alias $alias_name=\"$command\"" >> $rc
+				echo "alias created: $alias_name=\"$command\""
+			fi
+		else
+			echo "Invalid alias name."
+		fi
+
+	else
+		echo "Error: Invalid number of arguments passed.\nProper syntax: eza -a [ ALIAS ] [ COMMAND ]"
+	fi
+else
+	echo "Error: Unrecognized arguments."
+fi
