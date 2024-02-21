10db2ae22c0118f41bbafae68f68a22896fd19fc
Marc McAdoo <marcmcadoo123@gmail.com>
2024-02-21 09:03:28 -0500

    Added editing existing aliases command functionality

--- addalias.sh
+++ addalias.sh
@@ -6,6 +6,7 @@ valid_alias='^[a-zA-Z_][a-zA-Z0-9_]*$'
 # valid arg counts for input checking
 add_arg_count=3
 remove_arg_count=2
+edit_command_arg_count=3
 arg_count=$#
 # ensure at least one argument was passed
 if [ $# -eq 0 ]; then
@@ -47,6 +48,26 @@ elif [[ "$1" == "-r" ]]; then # remove alias
 	else
 		echo "Erro: Invalid nubmer of arguments passed.\nProper syntax: eza -r [ ALIAS ]"
 	fi
+elif [[ "$1" == "-cc" ]]; then # change command
+	if [ $# -eq $edit_command_arg_count ]; then
+		local alias_name=$2
+		local command=$3
+		local exists=$(grep -c "^alias $alias_name=" $rc)
+		if [ $exists -eq 0 ]; then
+			echo "No alias found with that name.\nAdding alias."
+		else
+			echo "Edited the following alias:"
+			grep "^alias $alias_name=" $rc
+			grep -v "^alias $alias_name=" $rc > temp_file && mv temp_file $rc
+
+		fi
+		echo "alias $alias_name=\"$command\"" >> $rc
+		echo "New alias:"
+		grep "^alias $alias_name=" $rc
+	else
+		echo "Error: Invalid number of arguments pass.\n Proper syntax: eza -cc [ ALIAS ] [ COMMAND ]"
+	fi
+
 else
 	echo "Error: Unrecognized arguments."
 fi
